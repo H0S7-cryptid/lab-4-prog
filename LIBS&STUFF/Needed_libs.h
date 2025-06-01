5c50298093ec5051a0b5be659fc9c3965b8e7e70
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 17:04:28 +0700

    Create Needed_libs.h

---
+++ LIBS&STUFF/Needed_libs.h
@@ -0,0 +1,16 @@
+#ifndef NEEDED_LIBS
+#define NEEDED_LIBS
+
+#include "MAPS.h"
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <sstream>   
+#include <stdexcept>
+#include <functional>
+#include <set>
+#include <iterator>
+
+#endif 
