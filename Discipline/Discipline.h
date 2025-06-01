1739bf1882ab7a0b49db1fc9a79e465d553ab7c4
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 22:43:47 +0700

    Update Discipline.h

--- Discipline/Discipline.h
+++ Discipline/Discipline.h
@@ -7,11 +7,12 @@ class Discipline {
 private:
 	short CCU;
 	std::string discName;
+	LvlOfEdu lvl;
 	bool tryName(std::string n);
 public:
 	// Конструкторы
 	Discipline();
-	Discipline(short CCU, std::string discName);
+	Discipline(short CCU, std::string discName, LvlOfEdu lvl);
 
 	// Оператор присваивания и сравнения
 	Discipline& operator=(const Discipline& d);
@@ -21,10 +22,12 @@ public:
 	// Геттеры
 	short getCurrentCU() const;
 	std::string getDiscName() const;
+	LvlOfEdu getLvl() const;
 
 	// Сеттеры значений
 	void setCurrentCU(short ccu);
 	void setDName(std::string name);
+	void setLvl(LvlOfEdu l);
 };
 
 
