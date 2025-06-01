6f37ca99c1b0d1a5d1bb7e1a42cf6ef49894b8ae
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 17:14:29 +0700

    Create CompOfCreators.cpp

---
+++ Composition Of Creator/CompOfCreators.cpp
@@ -0,0 +1,58 @@
+#include "CompOfCreators.h"
+
+void CompOfCreators::addCreator(Creator&& creator) {
+	creators.insert(creator);
+}
+
+void CompOfCreators::addCreator(const Creator& creator) {
+	creators.insert(creator);
+}
+
+void CompOfCreators::addCreator(const Person& name, StatusInUni& status) {
+	creators.insert(Creator{ name, status });
+}
+
+void CompOfCreators::deleteCreator(Creator&& creator) {
+	creators.erase(creator);
+}
+
+void CompOfCreators::deleteCreator(const Creator& creator) {
+	creators.erase(creator);
+}
+
+void CompOfCreators::deleteCreator(const Person& name, StatusInUni& status) {
+	creators.erase(Creator{ name, status });
+}
+
+void CompOfCreators::deleteCreator(int pos) {
+	auto it = creators.begin();
+	std::advance(it, pos);
+	creators.erase(it);
+}
+
+const std::set<Creator>& CompOfCreators::getCreators() const {
+	return creators;
+}
+
+std::ostream& operator<<(std::ostream& out, const CompOfCreators& s) {
+	out << "Current creators: ";
+	if (s.getCreators().empty()) {
+		out << "None";
+		return out;
+	}
+	out << std::endl;
+	int i = 1;
+	for (const auto& item : s.getCreators()) {
+		out << i << ". " << item;
+		i++;
+	}
+	return out;
+}
+
+CompOfCreators& CompOfCreators::operator=(const CompOfCreators& other) {
+	if (this != &other) {
+		creators = other.creators;
+	}
+	return *this;
+}
+
