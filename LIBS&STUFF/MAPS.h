39adab9c893fefa049ca6fcaef97fc1a0a778aa4
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 17:04:05 +0700

    Create MAPS.h

---
+++ LIBS&STUFF/MAPS.h
@@ -0,0 +1,12 @@
+#ifndef MAPS
+#define MAPS
+
+#include "ENUMS.h"
+#include <map>
+#include <string>
+
+extern std::map <LvlOfEdu, std::string> NameLvl;
+extern std::map <SylStatus, std::string> NameStatus;
+extern std::map <StatusInUni, std::string> NameUniStatus;
+
+#endif
