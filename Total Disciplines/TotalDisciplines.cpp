3460032576f6a5dfd620af8b7a8de70fe91bb186
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 22:48:00 +0700

    Update TotalDisciplines.cpp

--- Total Disciplines/TotalDisciplines.cpp
+++ Total Disciplines/TotalDisciplines.cpp
@@ -1,92 +1,37 @@
-#include "TotalDisciplines.h"
+#ifndef TOTAL_DISCIPLINES
+#define TOTAL_DISCIPLINES
 
-std::ostream& operator<<(std::ostream& out, const TotalDiscip& d) {
-	out << "Current Disceplines: ";
-	
-	if (d.getDisciplines().empty()) {
-		out << "None" << std::endl;
-		return out;
-	}
-	out << std::endl;
-	for (Discipline data : d.getDisciplines()) {
-		out << data;
-	}
-	return out;
-}
+#include "Discipline.h"
+#include "Needed_libs.h"
 
-std::vector <Discipline> TotalDiscip::getDisciplines() const {
-	return disciplines;
-}
+class TotalDiscip {
+private:
+	std::vector <Discipline> disciplines;
+public:
+	// Оператор вывода в поток
+	friend std::ostream& operator<<(std::ostream& out, const TotalDiscip& d);
 
-void TotalDiscip::addDisc(Discipline d) {
-	disciplines.push_back(d);
-	uniqueDisc();
-}
+	// Обращение к вектору значений
+	std::vector <Discipline> getDisciplines() const;
 
-void TotalDiscip::addDisc(short TCU, std::string name, LvlOfEdu lvl) {
-	disciplines.push_back(Discipline{ TCU, name, lvl });
-	uniqueDisc();
-}
+	// Методы добавления элементов в контейнер
+	void addDisc(Discipline d);
+	void addDisc(short TCU, std::string name, LvlOfEdu lvl);
 
-bool TotalDiscip::uniqueDisc() {
-	bool duplicatesFound = false;
+	// Метод проверки элементов контейнера на уникальность
+	bool uniqueDisc();
 
-	for (auto it = disciplines.begin(); it != disciplines.end(); ++it) {
-		for (auto jt = it + 1; jt != disciplines.end(); ) {
-			if (*it == *jt) {
-				jt = disciplines.erase(jt);
-				duplicatesFound = true;
-			}
-			else ++jt;
-		}
-	}
-	return !duplicatesFound;
-}
+	// Удаление по дисциплины значению
+	void deleteDisc(Discipline d);
 
-void TotalDiscip::deleteDisc(Discipline d) {
-	disciplines.erase(std::remove(disciplines.begin(), disciplines.end(), d), disciplines.end());
-}
+	// Установка полей 
+	void redoDiscCCU(short ccu, int pos);
+	void redoDiscName(std::string name, int pos);
+	void redoDisclvl(LvlOfEdu lvl, int pos);
 
-void TotalDiscip::redoDiscCCU(short ccu, int pos) {
-	if (pos > disciplines.size()) throw std::out_of_range("Position is beyond range!");
-	disciplines[pos].setCurrentCU(ccu);
-}
+	// Поиск существующих дисциплин
+	Discipline search(Discipline d);
+	Discipline search(short CCU, std::string name);
+};
 
-void TotalDiscip::redoDiscName(std::string name, int pos) {
-	if (pos > disciplines.size()) throw std::out_of_range("Position is beyond range!");
-	disciplines[pos].setDName(name);
-}
-
-void TotalDiscip::redoDisclvl(LvlOfEdu lvl, int pos) {
-	if (pos > disciplines.size()) throw std::out_of_range("Position is beyond range!");
-	disciplines[pos].setLvl(lvl);
-}
-
-Discipline TotalDiscip::search(Discipline d) {
-	for (const Discipline& find : disciplines) {
-
-		std::string name = find.getDiscName();
-		std::string name_to_find = d.getDiscName();
-		if ((name != name_to_find) || (name.empty() || name_to_find.empty()))
-			continue;
-
-		short CCU = find.getCurrentCU();
-		short CCU_to_find = d.getCurrentCU();
-		if (CCU != CCU_to_find) 
-			continue;
-
-		return find;
-	}
-}
-
-Discipline TotalDiscip::search(short CCU, std::string name) {
-	for (const Discipline& find : disciplines) {
-		if ((name != find.getDiscName()) || (name.empty() || find.getDiscName().empty()))
-			continue;
-
-		if (CCU != find.getCurrentCU())
-			continue;
-
-		return find;
-	}
-}
+#endif
