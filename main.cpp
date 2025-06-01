391f74b5669da1b858b4a96255332dcd76a73400
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 22:48:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,55 @@
 #include "SYLLABUS.h"
 
 int main() {
-	Syllabus s{ Cipher{"01","01","01"}, Master, "AFTV Syllabus", 37};
-
-	s.addCreators(Creator{ Person{"Natasha Dildakova Skibidishkina"}, Dean });
-	s.addCreators(Creator{});
-
-	s.addDiscipline(15, "Mathematical Analisys");
-	s.addDiscipline(10, "Linear Algebra");
-	s.addDiscipline(12, "OLKK");
-
-	std::cout << s;
+	Syllabus s{ Cipher{}, Bachelor, "AVTF Syllabus", 40, 5, 2 };
 
 	try {
-		s.Activate();
+		s.addDiscipline(Discipline{ 10, "Mathematical Analisys", Bachelor });
 	}
-	catch (std::exception& e) {
+	catch (std::logic_error& e) {
 		std::cout << e.what() << std::endl;
 	}
 
-	std::cout << std::endl;
-
 	try {
-		s.setLvlOfEdu(Bachelor);
+		s.addDiscipline(Discipline{ 10, "L25ar Alge5ra", Bachelor });
 	}
-	catch (std::exception& e) {
+	catch (std::logic_error& e) {
 		std::cout << e.what() << std::endl;
 	}
+
+	try {
+		s.addDiscipline(Discipline{ 10, "V#ct0r ana1isys", Bachelor });
+	}
+	catch (std::logic_error& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << s << std::endl;
+
+	Syllabus s1{ Cipher{}, Bachelor, "FTF Syllabus", 25, 7, 3 };
+
+	try {
+		s1.getDisciplines().redoDiscCCU(15, 1);
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		s1.addCreators(Creator{ Person{"Anna 4stin 34st3r"}, Dean });
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		s1.addCreators(Creator{ Person{"Anna ;s4in ::№st№r"}, Dean });
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << s1;
 
 	return 0;
 }
