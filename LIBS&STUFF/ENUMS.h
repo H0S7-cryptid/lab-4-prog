0dc7616dbe64a9fd1c38bf66e5ec6ae96bdf6385
host1l3 <timvitkulevskij@gmail.com>
2025-06-01 17:03:49 +0700

    Create ENUMS.h

---
+++ LIBS&STUFF/ENUMS.h
@@ -0,0 +1,8 @@
+#ifndef ENUMS
+#define ENUMS
+
+enum LvlOfEdu { Bachelor = 1, Master = 2, Postgraduate = 3, UNDEFL = -1 };
+enum SylStatus { Active = 1, Inactive = -1 };
+enum StatusInUni { Lecturer = 1, Professor, Dean, ViceRector, Rector, UNDEFSIN = -1 };
+
+#endif 
